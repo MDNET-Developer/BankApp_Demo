844972b75d53c05bef4ea95307d9c3ad677a04c0
MDNET-Developer <qarabagli.murad@mail.ru>
2022-10-27 11:38:31 +0400

    Generictype_update

---
+++ BankApp.Web/Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using BankApp.Web.Data.Context;
+using System.Collections.Generic;
+
+namespace BankApp.Web.Data.Interfaces
+{
+    public interface IGenericRepository<T> where T : class,new()
+    {
+        public void Create(T entity);
+
+        public void Remove(T entity);
+
+        public void Update(T entity);
+
+        public List<T> Getall();
+
+        public T GetbyId(int id);
+    }
+}
---
+++ BankApp.Web/Data/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using BankApp.Web.Data.Context;
+using BankApp.Web.Data.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankApp.Web.Data.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class,new()
+    {
+        private readonly BankContext _bankContext;
+
+        public GenericRepository(BankContext bankContext)
+        {
+            _bankContext = bankContext;
+        }
+        public void Create(T entity)
+        {
+            _bankContext.Set<T>().Add(entity);
+            _bankContext.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            _bankContext.Set<T>().Remove(entity);
+            _bankContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _bankContext.Set<T>().Update(entity);
+            _bankContext.SaveChanges();
+        }
+
+        public List<T> Getall()
+        {
+           return _bankContext.Set<T>().ToList();
+        }
+
+        public T GetbyId(int id)
+        {
+            return _bankContext.Set<T>().Find(id);
+        }
+    }
+}
